bbddb8dd2f0104d0bbf02f7e591aee24ef6a73ef
R1D3R175 <r1.d3.r175@gmail.com>
2026-03-11 15:30:12 +0100

    ✨ feat(ui/navbar): initialize navbar component

---
+++ libs/shared/ui/navbar/src/lib/navbar/navbar.spec.ts
@@ -0,0 +1,22 @@
+import { TestBed } from '@angular/core/testing';
+import { Navbar } from './navbar';
+import type { ComponentFixture } from '@angular/core/testing';
+
+describe('Navbar', () => {
+  let component: Navbar;
+  let fixture: ComponentFixture<Navbar>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Navbar],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Navbar);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
