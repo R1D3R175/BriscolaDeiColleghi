0ce04d5ac773307df701bdfb37b47f01765bf9ef
R1D3R175 <r1.d3.r175@gmail.com>
2026-03-16 01:30:40 +0100

    ♻️ refactor: fix linting errors

--- apps/client-e2e/playwright.config.ts
+++ apps/client-e2e/playwright.config.ts
@@ -3,7 +3,7 @@ import {nxE2EPreset} from '@nx/playwright/preset';
 import {defineConfig, devices} from '@playwright/test';
 
 // For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env.BASE_URL || 'http://localhost:4200';
+const baseUrl = process.env['BASE_URL'] ?? 'http://localhost:4200';
 
 /**
  * Read environment variables from file.
@@ -18,7 +18,8 @@ export default defineConfig({
   ...nxE2EPreset(import.meta.filename, {testDir: './src'}),
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL,
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    baseURL: baseUrl,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
