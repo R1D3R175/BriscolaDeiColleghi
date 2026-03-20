3dd29ca4a05efe7c9b977d464ff1c1ad09cb38ea
R1D3R175 <r1.d3.r175@gmail.com>
2026-03-20 19:11:19 +0100

    🔧 chore(stylelint): add nx-stylelint

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,8 +1,12 @@
-const lintedFiles = '*.{js,cjs,mjs,ts,cts,mts,json}';
+const eslintLintedFiles = '*.{js,cjs,mjs,ts,cts,mts,json}';
+const stylelintLintedFiles = '*.{css,scss}';
 const nxLint = files =>
   `pnpx nx affected --target=lint --fix --files=${files.join(',')}`;
+const nxStylelint = files =>
+  `pnpx nx affected --target=stylelint --fix --files=${files.join(',')}`;
 
 export default {
   lintedFiles: nxLint,
-  ['{apps,libs}/**/' + lintedFiles]: nxLint,
+  ['{apps,libs}/**/' + eslintLintedFiles]: nxLint,
+  ['{apps,libs}/**/' + stylelintLintedFiles]: nxStylelint,
 };
